6919a217ce3613236a5a3cbe96177cf261a3ed4a
Ryan <salvat36@gmail.com>
2023-05-22 16:01:36 -0500

    passing labs

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,8 @@
 def oxford_comma(items):
-    return None
+    if len(items) > 2:
+        slice = ", ".join(items[0:len(items)-1])
+        return ", and ".join([slice, items[-1]])
+    else:
+        return " and ".join(items)
+    
+
